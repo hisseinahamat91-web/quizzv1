9a0df49e8c6cc1d8448ad929851c67e37037136f
hisseinahamat91-web <hisseinahamat91@gmail.com>
2025-12-04 13:52:45 +0100

    js-fin

--- cript.js
+++ cript.js
@@ -1,1 +1,17 @@
-// je pas reissu
+
+
+let questionse = ["qui est le roi des animaux ?",
+     "quel est l'animal qui court le plus vite ?"];
+
+let reponsese = ["lion","guepard"];
+ 
+
+for (let i = 0; i <questions.length; i++) {
+    let reponseUtilisateur = prompt(questions[i]);
+ 
+    if (reponseUtilisateur === reponses[i]) {
+        alert("Bonne reponse");
+    } else {
+        alert("mauvaisse reponse");
+    }
+}
